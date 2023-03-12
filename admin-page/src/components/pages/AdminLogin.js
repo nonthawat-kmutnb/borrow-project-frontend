b8eaca896f073587cbf94463e57164447571b9fb
Supakron-Ch <97943384+SupakronQ@users.noreply.github.com>
2023-03-12 10:48:15 +0700

    Login page

--- admin-page/src/components/pages/AdminLogin.js
+++ admin-page/src/components/pages/AdminLogin.js
@@ -1,26 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HamburgerMenu from '../HamburgerMenu';
 import AnimatedPage from '../AnimatedPage'
+import Login from "../css/Login.css"
+import kmutnb from '../image/kmutnb.png';
+import ece from '../image/ece.png';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import swal from 'sweetalert';
+
 
 function App() {
+
+  // const classes = useStyles();
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async e =>  {
+  e.preventDefault();
+    if (true) {
+
+      swal("Success", "Login Succes", "success", {
+        buttons: false,
+        timer: 1000,
+      })
+      .then((value) => {
+        window.location.href = "/admin/rent";
+      });
+    } else {
+      swal("Failed", "Failed", "error");
+    }
+  }
+
   return (
+
     <AnimatedPage>
-      <div className="App">
-        <header className="App-header">
-          <h1>Admin Login</h1>
-          <HamburgerMenu />
-        </header>
-        <main>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          <p>Suspendisse potenti. Sed in justo in quam bibendum venenatis.</p>
-          <p>Donec quis felis eu sapien rutrum congue ac id mi.</p>
-          <p>Ut eget ex vel nisi semper bibendum ac a velit.</p>
-          <p>Aliquam blandit sapien vel lacus interdum rhoncus.</p>
-          <p>Phasellus gravida nisi sed nunc dapibus lacinia.</p>
-          <p>Proin id lectus vel erat aliquam bibendum.</p>
-          <p>Integer semper lorem vel erat facilisis, in auctor felis pulvinar.</p>
-        </main>
+      <div className='logo-container'>
+        <img src={kmutnb} alt="kmutnb" className="kmutnb-logo" />
+        <img src={ece} alt="ece" className="ece-logo" />
       </div>
+
+    <div class="float-container">
+
+      <div class="float-child_left" style={{backgroundColor: '#D2CFCA'}}>
+
+      <img className="LoginImg" alt="complex" src="https://www.smoengineer.net/wp-content/uploads/2014/08/front_of_eng_bld.jpg" 
+      style={{ alignItems: 'center',display: 'flex', justifyContent: 'center',height: '50vh',}}/>
+
+      </div>
+      
+      <div class="float-child_right" style={{backgroundColor: '#D2CFCA'}}>
+          <div className='float-Subchild'>
+            <h1>Login admin account</h1>
+          </div>
+          <div>
+
+              <form noValidate onSubmit={handleSubmit}>
+
+                <div className='float-Subchild'>
+                <label>Username :</label>
+                <input
+                  type="text"
+                  placeholder="Enter Username"
+                  onChange={e => setUserName(e.target.value)}/>
+                <label>password :</label>
+                <input
+                  type="text"
+                  placeholder="Enter password"
+                  onChange={e => setPassword(e.target.value)}/>
+                </div >
+                <div  className='float-Subchild' style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                  <Button type="submit" style={{backgroundColor: '#ff6004'}} color="neutral">Confirm</Button>
+                </div>
+
+              </form>
+            </div>
+      </div>
+      
+    </div>
+
     </AnimatedPage>
 
   );
